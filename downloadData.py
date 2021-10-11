ac67049fa18d6390ff875353558e8afe84748c36
PlutoCrashed <duitottheclayman@gmail.com>
2021-10-11 13:21:26 -0700

    User Interface Related Files

--- downloadData.py
+++ downloadData.py
@@ -1,6 +1,16 @@
+import numpy as np
 import pandas as pd
 import yfinance as yf
+import pickle
+from IPython.display import display
 from yahoofinancials import YahooFinancials
 
-spy = yf.download("spy")
-print(spy)
+data = yf.download("AMZN AAPL GOOG", start="2017-01-01", end="2017-04-30")
+
+# df = pd.DataFrame(data.data, columns=data.feature_names)
+
+display(data.to_string())
+stockData = open("stockData.txt", "w")
+
+
+stockData.write(data.to_string())
